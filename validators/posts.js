1bf25f313f1a114d1ecff6be317bb7131382a793
satnam <javascript.mspl@gmail.com>
2020-10-03 18:56:26 +0530

    fixes in validators

--- validators/posts.js
+++ validators/posts.js
@@ -20,7 +20,7 @@ const create = (req, res, next) => {
         log.end();
         return response.failure(res, "tags is required");
     }
-    if (!req.body.user {
+    if (!req.body.user) {
         log.end();
         return response.failure(res, "user id is required");
     }
