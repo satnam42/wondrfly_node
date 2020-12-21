702e00e2a62e44e6657d7f481d3b61524d8c46ed
satnam <javascript.mspl@gmail.com>
2020-12-21 21:09:09 +0530

    secuirty added for all api's

--- settings/express.js
+++ settings/express.js
@@ -3,11 +3,37 @@ const path = require("path");
 const cors = require("cors");
 const express = require("express");
 const bodyParser = require("body-parser");
+const allowUrl = require('config').get('allowUrl')
+const allowImageExtensions = require('config').get('allowExt')
+
+const whitelist = allowUrl
+
+let imageExtensions = allowImageExtensions.replace(/^'(.*)'$/, '$1');
+
+imageExtensions = imageExtensions.replace(/'/g, '"');
+
+imageExtensions = JSON.parse(imageExtensions);
+
+const corsOptionsDelegate = (req, callback) => {
+  
+  let corsOptions;
+
+  let isDomainAllowed = whitelist.indexOf(req.header('Origin')) !== -1;
+
+  let isExtensionAllowed = imageExtensions.some(imageExtension =>  req.path.endsWith(imageExtension))
+
+  if (isDomainAllowed || isExtensionAllowed) {
+      callback(null, true)
+  } else {
+    callback(new Error('Not allowed by CORS'))
+  }
+
+}
 
 const configure = async (app, logger) => {
   const log = logger.start("settings:express:configure");
   app.use(bodyParser.json());
-  app.use(cors());
+  app.use(cors(corsOptionsDelegate));
   app.use(
     bodyParser.urlencoded({
       extended: true
