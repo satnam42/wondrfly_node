0b10fed45a6c17ea62356dae2d39d666ece43614
gurjit <javascript.mspl@gmail.com>
2021-02-10 17:55:06 +0530

    image url set in chat mapper

--- mappers/conversation.js
+++ mappers/conversation.js
@@ -1,10 +1,15 @@
 'use strict'
+const baseUrl = require('config').get('image').baseUrl
 
 exports.toModel = (entity) => {
+    const mediaa = {
+        image: entity.media.image ? baseUrl + entity.media.image : ""
+    }
     const model = {
         msgFrom: entity.msgFrom,
         msgTo: entity.msgTo,
         msg: entity.msg,
+        media: mediaa,
         date: entity.createdOn,
     }
     return model
