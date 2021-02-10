8c7806cba1ab8a9c46c967894c2e4854bcb87b7e
gurjit <javascript.mspl@gmail.com>
2021-02-10 19:33:07 +0530

    required key solved in model

--- models/conversation.js
+++ models/conversation.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose')
 const chat = mongoose.Schema({
     msgFrom: { type: String, default: "", required: true },
     msgTo: { type: String, default: "", required: true },
-    msg: { type: String, default: "", required: true },
+    msg: { type: String, default: "" },
     image: { type: String, default: '' },
     room: { type: String, default: "", required: true },
     createdOn: { type: Date, default: Date.now }
