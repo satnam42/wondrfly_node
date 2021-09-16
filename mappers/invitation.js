d24d5da300c845ca8e651d4ff896ba0d90e5f3e0
gurjit <javascript.mspl@gmail.com>
2021-09-16 11:38:34 +0530

    is invited set in mapper of invitation

--- mappers/invitation.js
+++ mappers/invitation.js
@@ -5,6 +5,7 @@ exports.toModel = entity => {
         invitation: entity.id,
         email: entity.invitedToEmail ? entity.invitedToEmail : entity.user.email,
         name: entity.invitedToName ? entity.invitedToName : entity.user.firstName,
+        isInvited: entity.isInvited,
         status: entity.status,
         date: entity.createdOn
     }
