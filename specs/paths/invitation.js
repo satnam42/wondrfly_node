3bd87951f53a58b9dc4e015057ac65b8e0ea69f3
gurjit <javascript.mspl@gmail.com>
2021-09-30 12:26:40 +0530

    token is removed from ask to join api's swagger path

--- specs/paths/invitation.js
+++ specs/paths/invitation.js
@@ -13,14 +13,7 @@ module.exports = [
                     schema: {
                         $ref: "#/definitions/askToJoin"
                     }
-                },
-                // {
-                //     in: "header",
-                //     name: "x-access-token",
-                //     description: "token to access api",
-                //     required: true,
-                //     type: "string"
-                // }
+                }
             ],
             responses: {
                 default: {
